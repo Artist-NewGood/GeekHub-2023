4fe3f60ae4b2e71d411e35d541f551fb058bf63f
Oleksiy Diriavskiy <fildaiko@gmail.com>
2023-10-21 12:47:57 +0300

    update task_1.py

--- HT_3/task_1.py
+++ HT_3/task_1.py
@@ -2,14 +2,15 @@
     The list of tuples can be hardcoded. The "replacement" value is entered by user.
     The number of elements in the tuples must be different. """
 
-list_of_tuples = [(1, 2, 'bar'), (5, 'baz', 7, 19.3), ('geek', 'hub'), (True, 'False', None), ([1, 2], ['1', '2'])]
+list_of_tuples = [(1, 5, True), ("aaa", 2), (0,), ()]
+print(f'Original list: {list_of_tuples}')
 
 replacement_value = input('Enter replacement value: ')
 
 for i in range(len(list_of_tuples)):
-    list_of_tuples[i] = list(list_of_tuples[i])
-    list_of_tuples[i][-1] = replacement_value
-    list_of_tuples[i] = tuple(list_of_tuples[i])
+    if len(list_of_tuples[i]) != 0:
+        list_of_tuples[i] = list(list_of_tuples[i])
+        list_of_tuples[i][-1] = replacement_value
+        list_of_tuples[i] = tuple(list_of_tuples[i])
 
-print(list_of_tuples)
-
+print(f'Changed list: {list_of_tuples}')
