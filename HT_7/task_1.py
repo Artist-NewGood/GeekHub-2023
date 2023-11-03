c406bc01d411e8dbb956d811a82c1103d9883c56
Oleksiy Diriavskiy <fildaiko@gmail.com>
2023-11-03 13:53:00 +0200

    Add HT_7

---
+++ HT_7/task_1.py
@@ -0,0 +1,37 @@
+""" Створіть функцію, всередині якої будуть записано СПИСОК із п'яти
+    користувачів (ім'я та пароль). Функція повинна приймати три аргументи:
+    два - обов'язкових (<username> та <password>) і третій - необов'язковий
+    параметр <silent> (значення за замовчуванням - <False>).
+
+    Логіка наступна:
+        якщо введено правильну пару ім'я/пароль - вертається True;
+        якщо введено неправильну пару ім'я/пароль:
+            якщо silent == True - функція повертає False
+            якщо silent == False - породжується виключення LoginException (його також треба створити =)) """
+
+
+class LoginException(Exception):
+    pass
+
+
+def check_username_and_login(username: str, password: str, silent: bool = False) -> bool | str:
+    """Checking for login and password"""
+
+    user_data = [('Joey', 'polkfrt2#'), ('Brittney', 'qwerty'),
+                 ('Alex', 'trAmp'), ('Izabel', 'vbfgjsnwj#22'),
+                 ('WhoIAm', 'ukr2023')]
+    try:
+        if (username, password) in user_data:
+            return True
+        else:
+            raise LoginException
+    except LoginException:
+        if silent:
+            return False
+        return 'Login error'
+
+
+if __name__ == '__main__':
+    print(check_username_and_login('Joey', 'polkfrt2#'))
+    print(check_username_and_login('Joey1', 'polkfrt2#'))
+    print(check_username_and_login('Joey1', 'polkfrt2#', silent=True))
