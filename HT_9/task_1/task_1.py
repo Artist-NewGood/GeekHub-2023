bac92de36089c3c80b5a39b9413bb4f68de85d4a
Oleksiy Diriavskiy <fildaiko@gmail.com>
2023-11-12 20:49:50 +0200

    add HT_9

---
+++ HT_9/task_1/task_1.py
@@ -0,0 +1,35 @@
+""" Програма-світлофор.
+    Створити програму-емулятор світлофора для авто і пішоходів. Після запуска програми на екран виводиться в
+    лівій половині - колір автомобільного, а в правій - пішохідного світлофора. Кожну 1 секунду виводиться
+    поточні кольори. Через декілька ітерацій - відбувається зміна кольорів - логіка така сама як і в
+    звичайних світлофорах (пішоходам зелений тільки коли автомобілям червоний). """
+
+from time import sleep
+
+
+def color() -> tuple:
+    """Generator to control traffic light colors.
+       Returns tuples with colors for car and pedestrians."""
+
+    colors = (
+              ('green', 'red'),
+              ('yellow', 'red'),
+              ('red', 'green'),
+              ('yellow', 'red')
+             )
+    while True:
+        yield from colors
+
+
+def traffic_light() -> None:
+    """The function iterates through the colors of a traffic light,
+    Printing the current colors for both cars and pedestrians."""
+
+    for color_car, color_man in color():
+        for _ in range(3):
+            print(f'{color_car} {color_man}')
+            sleep(1)
+
+
+if __name__ == '__main__':
+    traffic_light()
