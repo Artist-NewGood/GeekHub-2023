8840fa64fa397e9a5f1585aa30b67e260efd96a1
Oleksiy Diriavskiy <79664389+Artist-NewGood@users.noreply.github.com>
2023-11-23 15:35:16 +0200

    Update task_1.py

--- HT_11/task_1.py
+++ HT_11/task_1.py
@@ -29,25 +29,25 @@ class Calc:
     def add(self, number_1, number_2):
         self.result = number_1 + number_2
         self.operation()
-        return self.last_result
+        return self.result
         
     def multi(self, number_1, number_2):
         self.result = number_1 * number_2
         self.operation()
-        return self.last_result
+        return self.result
         
     def sub(self, number_1, number_2):
         self.result = number_1 - number_2
         self.operation()
-        return self.last_result
+        return self.result
         
     def div(self, number_1, number_2):
-         try:
+        try:
             self.result = number_1 / number_2
         except ZeroDivisionError as err:
             self.result = err
         self.operation()
-        return self.last_result
+        return self.result
     
     def operation(self):
         self.last_result = self.current_result
