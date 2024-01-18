135d9983fea54de24810e7e5a63e640d7911885e
Oleksiy Diriavskiy <79664389+Artist-NewGood@users.noreply.github.com>
2024-01-18 12:59:54 +0200

    Delete HT_20 directory

--- HT_20/store/product/services/utils.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import messages
-
-
-def checking_access_rights(request):
-    if (not request.user.is_superuser) or (not request.user.is_superuser):
-        messages.error(request, "Функція недоступна для звичайного або незалогіненого користувача.")
-        return True
-    return False
